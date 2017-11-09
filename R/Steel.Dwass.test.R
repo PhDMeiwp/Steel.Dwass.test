d06e2acc53c5744f234d6f75e9b65e481823dacc
PhDMeiwp <meiweipingg@163.com>
2017-11-09 17:19:52 +0900

    Source file update

--- R/Steel.Dwass.test.R
+++ R/Steel.Dwass.test.R
@@ -11,6 +11,7 @@
 #' x <- c(6.9, 7.5, 8.5, 8.4, 8.1, 8.7, 8.9, 8.2, 7.8, 7.3, 6.8,9.6, 9.4, 9.5, 8.5, 9.4, 9.9, 8.7, 8.1, 7.8, 8.8,5.7, 6.4, 6.8, 7.8, 7.6, 7.0, 7.7, 7.5, 6.8, 5.9,7.6, 8.7, 8.5, 8.5, 9.0, 9.2, 9.3, 8.0, 7.2, 7.9, 7.8)
 #' group <- rep(1:4, c(11, 10, 10, 11))
 #' Steel.Dwass(x, group)
+#' @author Robert George Douglas Steel,Shigenobu Aoki,Weiping Mei
 Steel.Dwass <- function(x,group) 
 {
   OK <- complete.cases(x, group)
